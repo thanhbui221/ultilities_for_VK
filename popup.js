03545e0a3cd11da7f6ae7e634af32f2bd71dd37d
jjjaberion <thanhbui22198@gmail.com>
2021-08-16 09:45:43 +0700

    first test

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 let vk_privacy = document.getElementById("VKDiv");
-let features = document.getElementById("Features");
+//let features = document.getElementById("Features");
 
 var portSettings = chrome.runtime.connect({name: "settings"});
 
@@ -13,38 +13,46 @@ const vk_options = {"block_chat_seen": "Block 'seen' feature of chat ", "block_t
   "block_story_seen": "Block 'seen' feature of story on VK"};
 
 
-for (let key in vk_options){
-  let div = document.createElement('div');
-  div.classList.add("form-check");
-  let input = document.createElement('input');
-  input.classList.add('form-check-input');
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("id", key);
-  let label = document.createElement('label');
-  label.classList.add('form-check-label');
-  let text = document.createTextNode(vk_options[key]);
-  label.appendChild(text);
-  div.appendChild(input);
-  div.appendChild(label);
-  vk_privacy.appendChild(div);
-}
 
-for(let key in settings){
-  chrome.storage.sync.get([key], function(old_settings){
-    document.getElementById(key).checked = old_settings[key];
-  })
-}
+(init = () => {
+  for (let key in vk_options){
+    let div = document.createElement('div');
+    div.classList.add("form-check");
+    let input = document.createElement('input');
+    input.classList.add('form-check-input');
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("id", key);
+    let label = document.createElement('label');
+    label.classList.add('form-check-label');
+    let text = document.createTextNode(vk_options[key]);
+    label.appendChild(text);
+    div.appendChild(input);
+    div.appendChild(label);
+    vk_privacy.appendChild(div);
+  }
+})();
 
-let myButton = document.getElementById('myButton');
-myButton.onclick = function(element){
+(sync = () => {
+  for(let key in settings){
+    chrome.storage.sync.get([key], (old_settings) => {
+      document.getElementById(key).checked = old_settings[key];
+    })
+  }
+})();
+
+
+
+let myButton = document.getElementById('myButton').addEventListener('click', () => {
   for (let key in settings){
     settings[key] = document.getElementById(key).checked
   }
 
   portSettings.postMessage(settings);
   for (let key in settings){
-    chrome.storage.sync.set({key: settings[key]}, function(){
+    chrome.storage.sync.set({key: settings[key]}, () => {
       console.log(key, settings[key]);
     });
   }
-};
+});
+
+
